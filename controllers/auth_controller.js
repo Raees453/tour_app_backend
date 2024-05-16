c7ae9b49880b7b34b09ec8894fbeb2f2fe97652f
Ali Wajdan Pasha <raeesali453@gmail.com>
2024-05-17 00:30:39 +0500

    Login/Signup APIs Added.

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,41 +1,60 @@
-
 const bcrypt = require('bcrypt');
 
 const asyncHandler = require('../utils/async_handler');
-const { PrismaClient } = require('@prisma/client');
+const {PrismaClient} = require('@prisma/client');
 
 const Exception = require('../utils/exception');
 
 const prisma = new PrismaClient();
 
+// first, last, phone, email, role, password
 exports.login = asyncHandler(async (req, res, next) => {
-  res.status(200).json({});
+
+  const {email, password, role} = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email, role
+    }
+  });
+
+  if (!user) return next(new Exception('User does not exist', 404));
+
+  const isPasswordSame = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordSame) return next(new Exception('Password does not match', 401));
+  
+  user.otp = undefined;
+  user.otpExpireAt = undefined;
+  user.password = undefined;
+  user.passwordChangedAt = undefined;
+
+  res.status(200).json({
+    status: true,
+    body: user,
+  });
 
   next();
 });
 
 exports.signUp = asyncHandler(async (req, res, next) => {
 
-  let { email, password, confirmPassword, role } = req.body;
+  let {email, password, role, firstName, lastName, phone} = req.body;
 
-  if (!email || !password || !confirmPassword || !role) {
-    return next(new Exception('Please provide email, password, confirmPassword and role.', 400));
+  if (!email || !password || !role || !firstName || !lastName || !phone || !role) {
+    return next(new Exception('Please provide email, password, role, firstName, lastName & phone', 400));
   }
 
-  if (password !== confirmPassword) {
-    return next(new Exception('Password and Confirm Password do not match', 400));
-  }
-
-  password = bcrypt.hash(password, 10);
+  password = await bcrypt.hash(password, 10);
 
   const user = await prisma.user.create({
-    data: { email, password, role },
+    data: {email, password, role, firstName, lastName, phone},
   });
 
   user.otp = undefined;
   user.otpExpireAt = undefined;
-  // user.password = undefined;
-  // user.passwordChangedAt = undefined;
+  user.password = undefined;
+  user.passwordChangedAt = undefined;
 
   res.status(201).json({
     status: true,
@@ -49,7 +68,7 @@ exports.signUp = asyncHandler(async (req, res, next) => {
 
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
-  const { email, password } = req.body;
+  const {email, password} = req.body;
 
   if (!email || !password) return next(new Exception('Please provide email & password', 400));
 
